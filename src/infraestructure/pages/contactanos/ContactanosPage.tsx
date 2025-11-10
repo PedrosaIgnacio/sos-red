cb64112842de856506feedcdcbf3e35aa313f099
PedrosaIgnacio <ignaciopedrosa1998@gmail.com>
2025-11-10 09:10:59 -0300

    fix typo

--- src/infraestructure/pages/contactanos/ContactanosPage.tsx
+++ src/infraestructure/pages/contactanos/ContactanosPage.tsx
@@ -5,7 +5,7 @@ export const ContactanosPage = () => {
   return (
     <div className="w-full flex flex-col bg-[#EEE3D0]">
       <section className="w-full flex my-10 px-28 flex-col gap-8 items-center">
-        <h1 className="text-[#FF4020] font-bold text-7xl">Ponete en contacto</h1>
+        <h1 className="text-[#FF4020] font-bold text-7xl">Ponte en contacto</h1>
         <ContactanosActionButtonsWrapper />
       </section>
       <section className="w-full flex my-10 px-28 flex-col gap-8 items-center">

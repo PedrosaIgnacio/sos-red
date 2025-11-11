186b4147e55245c33b5f087197530a43c1ce2367
PedrosaIgnacio <ignaciopedrosa1998@gmail.com>
2025-11-11 13:21:45 -0300

    update whatsapp button display

--- src/infraestructure/components/WhatsappFloatingButton.tsx
+++ src/infraestructure/components/WhatsappFloatingButton.tsx
@@ -1,29 +1,59 @@
+// import { FaWhatsapp } from 'react-icons/fa';
+
+// export const WhatsappFloatingButton = () => {
+//   return (
+//     <button
+//       className="
+//         absolute
+//         bottom-6
+//         right-6
+//         bg-green-500
+//         hover:bg-green-600
+//         text-white
+//         rounded-full
+//         p-3
+//         cursor-pointer
+//         shadow-lg
+//         transition-all
+//         duration-300
+//         flex
+//         items-center
+//         justify-center
+//       "
+//       onClick={() => window.open('https://wa.me/5491128080012', '_blank')}
+//       aria-label="Contactar por WhatsApp"
+//     >
+//       <FaWhatsapp size={32} />
+//     </button>
+//   );
+// };
+
 import { FaWhatsapp } from 'react-icons/fa';
 
 export const WhatsappFloatingButton = () => {
   return (
     <button
       className="
-        absolute 
-        bottom-6 
-        right-6 
-        bg-green-500 
-        hover:bg-green-600 
-        text-white 
-        rounded-full 
-        p-3
-        cursor-pointer
-        shadow-lg 
-        transition-all 
-        duration-300 
-        flex 
-        items-center 
+        fixed
+        bottom-6
+        right-6
+        bg-green-500
+        hover:bg-green-600
+        text-white
+        rounded-full
+        p-4
+        shadow-lg
+        transition-all
+        duration-300
+        flex
+        items-center
         justify-center
+        z-50
       "
-      onClick={() => window.open('https://wa.me/5491128080012', '_blank')}
+      onClick={() => window.open('https://wa.me/549XXXXXXXXXX', '_blank')}
       aria-label="Contactar por WhatsApp"
     >
-      <FaWhatsapp size={32} />
+      <FaWhatsapp size={28} />
     </button>
   );
 };
